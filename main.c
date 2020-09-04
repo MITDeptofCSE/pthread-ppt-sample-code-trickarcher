bd33d2e3ad3f1469994a48df3f009f0e4c61f3ed
trickarcher <anishbiswas271@gmail.com>
2020-09-04 22:08:55 +0530

    Added pthread example code

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
-#include<stdio.h>
-int main()
-{
-  printf("Hello");
-  //Add your code below
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum;
+
+void runner(void* param);
+
+int main(int argc, char** argv) {
+	pthread_t tid;
+	pthread_attr_t attr;
+
+	if(argc != 2) {
+		fprintf(stderr, "usage a.out <integer value>\n");
+		return -1;
+	}
+
+	int arg_int = atoi(argv[1]);
+	if(arg_int < 0) {
+		fprintf(stderr, "%d must be >= 0\n", arg_int);
+		return -1;
+	}
+
+	pthread_attr_init(&attr);
+
+	pthread_create(&tid, &attr, (void*)runner, argv[1]);
+
+	pthread_join(tid, NULL);
+
+	printf("sum = %d\n:", sum);
+}
+
+void runner(void* param) {
+	int i, upper = atoi(param);
+	sum = 0;
+
+	for(i = 1; i <= upper; i++)
+		sum += i;
+	pthread_exit(0);
 }
